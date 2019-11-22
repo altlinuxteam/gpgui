2e8945c556c084855def0b04c9b99965aad0426e
Igor "NIR" Chudov <nir@nir.org.ru>
2019-11-22 12:14:59 +0400

    Function to add Registy.pol entry to table introduced

--- src/gui/MainWindow.cpp
+++ src/gui/MainWindow.cpp
@@ -29,11 +29,30 @@ void qgui::MainWindow::createStatusBar() {
     this->statusBar()->showMessage(tr("Ready"));
 }
 
+namespace {
+void preg_entry2table(QTableWidget *regpol_table, preg::entry &pentry, size_t rownum) {
+    std::string regtype = std::string(preg::regtype2str(pentry.type));
+    std::string val = std::to_string(pentry.value);
+    QTableWidgetItem *vname = new QTableWidgetItem(pentry.value_name.c_str());
+    QTableWidgetItem *kname = new QTableWidgetItem(pentry.key_name.c_str());
+    QTableWidgetItem *vtype = new QTableWidgetItem(regtype.c_str());
+    QTableWidgetItem *vval = new QTableWidgetItem(val.c_str());
+    regpol_table->insertRow(regpol_table->rowCount());
+    regpol_table->setItem(rownum, 0, vname);
+    regpol_table->setItem(rownum, 1, kname);
+    regpol_table->setItem(rownum, 2, vtype);
+    regpol_table->setItem(rownum, 3, vval);
+}
+}
+
 qgui::MainWindow::MainWindow(QWidget *parent)
 //: QMainWindow(parent)
 {
     preg::preg_parser *test_regpol = new preg::preg_parser("Registry.pol");
     preg::entry pentry = test_regpol->get_next_entry();
+    preg::entry pentry2 = test_regpol->get_next_entry();
+    preg::entry pentry3 = test_regpol->get_next_entry();
+    preg::entry pentry4 = test_regpol->get_next_entry();
 
     QVBoxLayout *layout_regpol_editor = new QVBoxLayout;
     QVBoxLayout *layout_gpo_editor = new QVBoxLayout;
@@ -46,16 +65,10 @@ qgui::MainWindow::MainWindow(QWidget *parent)
     tw->widget(0)->setLayout(layout_regpol_editor);
     layout_regpol_editor->addWidget(regpol_table);
 
-    std::string regtype = std::string(preg::regtype2str(pentry.type));
-    std::string val = std::to_string(pentry.value);
-    QTableWidgetItem *vname = new QTableWidgetItem(pentry.value_name.c_str());
-    QTableWidgetItem *kname = new QTableWidgetItem(pentry.key_name.c_str());
-    QTableWidgetItem *vtype = new QTableWidgetItem(regtype.c_str());
-    QTableWidgetItem *vval = new QTableWidgetItem(val.c_str());
-    regpol_table->setItem(0, 0, vname);
-    regpol_table->setItem(0, 1, kname);
-    regpol_table->setItem(0, 2, vtype);
-    regpol_table->setItem(0, 3, vval);
+    preg_entry2table(regpol_table, pentry, 0);
+    preg_entry2table(regpol_table, pentry2, 1);
+    preg_entry2table(regpol_table, pentry3, 2);
+    preg_entry2table(regpol_table, pentry4, 3);
 
     QVBoxLayout *layout = new QVBoxLayout;
     layout->addWidget(tw);
