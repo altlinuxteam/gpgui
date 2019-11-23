102c1415746dad8bc0116bc44efc97b9bbaa02d3
Igor "NIR" Chudov <nir@nir.org.ru>
2019-11-23 22:25:26 +0400

    libgptbackend tests expanded

--- src/libgptbackend/test/main.cpp
+++ src/libgptbackend/test/main.cpp
@@ -6,25 +6,32 @@
 #include "preg_parser.h"
 
 #include <string>
-#include <iostream>
-#include <fstream>
 
 TEST_CASE("It is possible to convert UTF-8 string to UTF-16LE", "[iconv_wrapper]") {
     std::string test_str_utf8 = "Software\\BaseALT\\Policies\\Control";
-    std::string test_str_utf16le{ 'S', '\x00', 'o', '\x00', 'f', '\x00', 't', '\x00',
-        'w', '\x00', 'a', '\x00', 'r', '\x00', 'e', '\x00' };
+    std::string test_str_utf16le{
+'S', '\x00', 'o',  '\x00', 'f',  '\x00', 't', '\x00', 'w',  '\x00', 'a', '\x00',
+'r', '\x00', 'e',  '\x00', '\\', '\x00', 'B', '\x00', 'a',  '\x00', 's', '\x00',
+'e', '\x00', 'A',  '\x00', 'L',  '\x00', 'T', '\x00', '\\', '\x00', 'P', '\x00',
+'o', '\x00', 'l',  '\x00', 'i',  '\x00', 'c', '\x00', 'i',  '\x00', 'e', '\x00',
+'s', '\x00', '\\', '\x00', 'C',  '\x00', 'o', '\x00', 'n',  '\x00', 't', '\x00',
+'r', '\x00', 'o',  '\x00', 'l',  '\x00'
+    };
     gptbackend::iconv_wrapper iwrapper("UTF-8", "UTF-16LE");
-    gptbackend::iconv_wrapper iwrapper2("UTF-16LE", "UTF-8");
-    std::cout << "1 " << iwrapper.convert(test_str_utf8) << std::endl;
-    std::cout << "2 " << iwrapper2.convert(test_str_utf16le) << std::endl;
-    //CHECK(false);
+    REQUIRE(test_str_utf16le == iwrapper.convert(test_str_utf8));
 }
 
-TEST_CASE("Registry.pol can be parsed", "[preg]") {
-    preg::preg_parser *test_regpol = new preg::preg_parser("Registry.pol");
-    test_regpol->get_next_entry();
-    test_regpol->get_next_entry();
-    test_regpol->get_next_entry();
-    test_regpol->get_next_entry();
+TEST_CASE("It is possile to convert UTF-16LE string to UTF-8", "[iconv_wrapper]") {
+    std::string test_str_utf8 = "Software\\BaseALT\\Policies\\Control";
+    std::string test_str_utf16le{
+'S', '\x00', 'o',  '\x00', 'f',  '\x00', 't', '\x00', 'w',  '\x00', 'a', '\x00',
+'r', '\x00', 'e',  '\x00', '\\', '\x00', 'B', '\x00', 'a',  '\x00', 's', '\x00',
+'e', '\x00', 'A',  '\x00', 'L',  '\x00', 'T', '\x00', '\\', '\x00', 'P', '\x00',
+'o', '\x00', 'l',  '\x00', 'i',  '\x00', 'c', '\x00', 'i',  '\x00', 'e', '\x00',
+'s', '\x00', '\\', '\x00', 'C',  '\x00', 'o', '\x00', 'n',  '\x00', 't', '\x00',
+'r', '\x00', 'o',  '\x00', 'l',  '\x00'
+    };
+    gptbackend::iconv_wrapper iwrapper2("UTF-16LE", "UTF-8");
+    REQUIRE(test_str_utf8 == iwrapper2.convert(test_str_utf16le));
 }
 
---
+++ src/libgptbackend/test/test_preg_data.cpp
@@ -0,0 +1,142 @@
+#include <catch2/catch.hpp>
+#include "config.h"
+
+#include "preg_data.h"
+
+TEST_CASE("Convert 0 to REG_NONE", "[regtype2str]") {
+    uint16_t regtype = 0;
+    REQUIRE("REG_NONE" == preg::regtype2str(regtype));
+}
+
+TEST_CASE("Convert 1 to REG_SZ", "[regtype2str]") {
+    uint16_t regtype = 1;
+    REQUIRE("REG_SZ" == preg::regtype2str(regtype));
+}
+
+TEST_CASE("Convert 2 to REG_EXPAND_SZ", "[regtype2str]") {
+    uint16_t regtype = 2;
+    REQUIRE("REG_EXPAND_SZ" == preg::regtype2str(regtype));
+}
+
+TEST_CASE("Convert 3 to REG_BINARY", "[regtype2str]") {
+    uint16_t regtype = 3;
+    REQUIRE("REG_BINARY" == preg::regtype2str(regtype));
+}
+
+TEST_CASE("Convert 4 to REG_DWORD_LITTLE_ENDIAN", "[regtype2str]") {
+    uint16_t regtype = 4;
+    REQUIRE("REG_DWORD_LITTLE_ENDIAN" == preg::regtype2str(regtype));
+}
+
+TEST_CASE("Convert 5 to REG_DWORD_BIG_ENDIAN", "[regtype2str]") {
+    uint16_t regtype = 5;
+    REQUIRE("REG_DWORD_BIG_ENDIAN" == preg::regtype2str(regtype));
+}
+
+TEST_CASE("Convert 6 to REG_LINK", "[regtype2str]") {
+    uint16_t regtype = 6;
+    REQUIRE("REG_LINK" == preg::regtype2str(regtype));
+}
+
+TEST_CASE("Convert 7 to REG_MULTI_SZ", "[regtype2str]") {
+    uint16_t regtype = 7;
+    REQUIRE("REG_MULTI_SZ" == preg::regtype2str(regtype));
+}
+
+TEST_CASE("Convert 8 to REG_RESOURCE_LIST", "[regtype2str]") {
+    uint16_t regtype = 8;
+    REQUIRE("REG_RESOURCE_LIST" == preg::regtype2str(regtype));
+}
+
+TEST_CASE("Convert 9 to REG_FULL_RESOURCE_DESCRIPTOR", "[regtype2str]") {
+    uint16_t regtype = 9;
+    REQUIRE("REG_FULL_RESOURCE_DESCRIPTOR" == preg::regtype2str(regtype));
+}
+
+TEST_CASE("Convert 10 to REG_RESOURCE_REQUIREMENTS_LIST", "[regtype2str]") {
+    uint16_t regtype = 10;
+    REQUIRE("REG_RESOURCE_REQUIREMENTS_LIST" == preg::regtype2str(regtype));
+}
+
+TEST_CASE("Convert 11 to REG_QWORD", "[regtype2str]") {
+    uint16_t regtype = 11;
+    REQUIRE("REG_QWORD" == preg::regtype2str(regtype));
+}
+
+TEST_CASE("Convert 12 to REG_QWORD_LITTLE_ENDIAN", "[regtype2str]") {
+    uint16_t regtype = 12;
+    REQUIRE("REG_QWORD_LITTLE_ENDIAN" == preg::regtype2str(regtype));
+}
+
+TEST_CASE("Convert REG_NONE to 0", "[str2regtype]") {
+    const uint16_t regtype = 0;
+    std::string regname = "REG_NONE";
+    REQUIRE(preg::str2regtype(regname) == regtype);
+}
+
+TEST_CASE("Convert REG_SZ to 1", "[str2regtype]") {
+    const uint16_t regtype = 1;
+    std::string regname = "REG_SZ";
+    REQUIRE(preg::str2regtype(regname) == regtype);
+}
+
+TEST_CASE("Convert REG_EXPAND_SZ to 2", "[str2regtype]") {
+    const uint16_t regtype = 2;
+    std::string regname = "REG_EXPAND_SZ";
+    REQUIRE(preg::str2regtype(regname) == regtype);
+}
+
+TEST_CASE("Convert REG_BINARY to 3", "[str2regtype]") {
+    const uint16_t regtype = 3;
+    std::string regname = "REG_BINARY";
+    REQUIRE(preg::str2regtype(regname) == regtype);
+}
+
+TEST_CASE("Convert REG_DWORD_LITTLE_ENDIAN to 4", "[str2regtype]") {
+    const uint16_t regtype = 4;
+    std::string regname = "REG_DWORD_LITTLE_ENDIAN";
+    REQUIRE(preg::str2regtype(regname) == regtype);
+}
+
+TEST_CASE("Convert REG_DWORD_BIG_ENDIAN to 5", "[str2regtype]") {
+    const uint16_t regtype = 5;
+    std::string regname = "REG_DWORD_BIG_ENDIAN";
+    REQUIRE(preg::str2regtype(regname) == regtype);
+}
+
+TEST_CASE("Convert REG_LINK to 6", "[str2regtype]") {
+    const uint16_t regtype = 6;
+    std::string regname = "REG_LINK";
+    REQUIRE(preg::str2regtype(regname) == regtype);
+}
+
+TEST_CASE("Convert REG_MULTI_SZ to 7", "[str2regtype]") {
+    const uint16_t regtype = 7;
+    std::string regname = "REG_MULTI_SZ";
+    REQUIRE(preg::str2regtype(regname) == regtype);
+}
+
+TEST_CASE("Convert REG_RESOURCE_LIST to 8", "[str2regtype]") {
+    const uint16_t regtype = 9;
+    std::string regname = "REG_FULL_RESOURCE_DESCRIPTOR";
+    REQUIRE(preg::str2regtype(regname) == regtype);
+}
+
+TEST_CASE("Convert REG_RESOURCE_REQUIREMENTS_LIST to 10", "[str2regtype]") {
+    const uint16_t regtype = 10;
+    std::string regname = "REG_RESOURCE_REQUIREMENTS_LIST";
+    REQUIRE(preg::str2regtype(regname) == regtype);
+}
+
+TEST_CASE("Convert REG_QWORD to 11", "[str2regtype]") {
+    const uint16_t regtype = 11;
+    std::string regname = "REG_QWORD";
+    REQUIRE(preg::str2regtype(regname) == regtype);
+}
+
+TEST_CASE("Convert REG_QWORD_LITTLE_ENDIAN to 12", "[str2regtype]") {
+    const uint16_t regtype = 12;
+    std::string regname = "REG_QWORD_LITTLE_ENDIAN";
+    REQUIRE(preg::str2regtype(regname) == regtype);
+}
+
